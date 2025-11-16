d720f0cfcd61048a0223420286f0dbc43b49d8ab
SRINIVASA V <seenavamshi@gmail.com>
2025-11-16 17:36:59 +0530

    Create variables.tf

---
+++ terraform-vm/variables.tf
@@ -0,0 +1,16 @@
+variable "subscription_id" {}
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
+
+variable "resource_group" {
+  default = "rg-terraform-demo"
+}
+
+variable "location" {
+  default = "East US"
+}
+
+variable "vm_name" {
+  default = "tf-vm"
+}
