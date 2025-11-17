888ba4e0adc385ed95e4b99e2f4e0d2c82ba3daa
SRINIVASA V <seenavamshi@gmail.com>
2025-11-17 19:21:53 +0530

    Update main.tf

--- terraform-vm/main.tf
+++ terraform-vm/main.tf
@@ -1,17 +1,24 @@
 resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group
+  name     = var.resource_group_name
   location = var.location
 }
 
+resource "azurerm_public_ip" "pip" {
+  name                = "${var.vm_name}-pip"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+}
+
 resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet-demo"
+  name                = "${var.vm_name}-vnet"
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "subnet" {
-  name                 = "subnet1"
+  name                 = "${var.vm_name}-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
@@ -30,26 +37,19 @@ resource "azurerm_network_interface" "nic" {
   }
 }
 
-resource "azurerm_public_ip" "pip" {
-  name                = "${var.vm_name}-pip"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static"
-}
-
 resource "azurerm_linux_virtual_machine" "vm" {
   name                = var.vm_name
   resource_group_name = azurerm_resource_group.rg.name
   location            = var.location
-  size                = "Standard_B1s"
-  admin_username      = "azureuser"
+  size                = var.vm_size
+  admin_username      = var.admin_username
   network_interface_ids = [
     azurerm_network_interface.nic.id
   ]
 
   admin_ssh_key {
-    username   = "azureuser"
-    public_key = file("~/.ssh/id_rsa.pub")
+    username   = var.admin_username
+    public_key = file(var.ssh_public_key_path)
   }
 
   os_disk {
