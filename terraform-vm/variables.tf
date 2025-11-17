856a671b76df6d5ff22eca643636b0cd27e4388d
SRINIVASA V <seenavamshi@gmail.com>
2025-11-17 19:27:36 +0530

    Update variables.tf

--- terraform-vm/variables.tf
+++ terraform-vm/variables.tf
@@ -1,17 +1,57 @@
-variable "subscription_id" {}
-variable "client_id" {}
-variable "client_secret" {}
-variable "tenant_id" {}
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
 
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+}
 
-variable "resource_group" {
-  default = "rg-terraform-demo"
+variable "client_id" {
+  description = "Azure Client / Service Principal ID"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "Azure Client Secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group to create"
+  type        = string
+  default     = "rg-terraform-demo"
 }
 
 variable "location" {
-  default = "East US"
+  description = "Azure region / location"
+  type        = string
+  default     = "East US"
 }
 
 variable "vm_name" {
-  default = "tf-vm"
+  description = "Name of the VM"
+  type        = string
+  default     = "tf-vm"
 }
+
+variable "vm_size" {
+  description = "Size of the VM"
+  type        = string
+  default     = "Standard_B1s"
+}
+
+variable "admin_username" {
+  description = "Admin user name for the VM"
+  type        = string
+  default     = "azureuser"
+}
+
+variable "ssh_public_key_path" {
+  description = "Path to the SSH public key (for Linux VM)"
+  type        = string
+  default     = "~/.ssh/id_rsa.pub"
+}
+
